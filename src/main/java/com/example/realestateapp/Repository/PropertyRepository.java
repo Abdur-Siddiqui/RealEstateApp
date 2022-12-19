3558ef7d13d32dba85d208bcbceacc007c0b7420
Abdur Siddiqui <abdursiddiqui2003@gmail.com>
2022-12-19 16:48:10 -0500

    adding chnages to reposiotry classes

--- src/main/java/com/example/realestateapp/Repository/PropertyRepository.java
+++ src/main/java/com/example/realestateapp/Repository/PropertyRepository.java
@@ -1,4 +1,10 @@
 package com.example.realestateapp.Repository;
 
-public interface PropertyRepository {
+import com.example.realestateapp.Entity.Property;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PropertyRepository extends CrudRepository<Property, Long> {
+
 }
---
+++ src/main/java/com/example/realestateapp/Service/BookingService.java
@@ -0,0 +1,4 @@
+package com.example.realestateapp.Service;
+
+public class BookingService {
+}
